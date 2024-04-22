dab6f377b98b2c01b1540e2c06fa11431d8a863f
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-04-22 12:09:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-function firstNonRepeatedChar(str) {
+function firstNonRepeatedChar(nstr) {
  // Write your code here
+	let str = nstr.split("").sort().join("")
 	for(let i=0;i<str.length;i++){
 		if((i+1<str.length) && (i-1>=0) && (str[i]!=str[i+1]) && (str[i]!=str[i-1])) return str[i]
 		else if((i-1<0) &&(i+1<str.length) && (str[i]!=str[i+1])) return str[i]
